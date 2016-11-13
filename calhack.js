b8c7896359d6f309b946873c37772aa93091d331
Alexis Chen <chenming@usc.edu>
2016-11-12 21:53:55 -0800

    some functionality for data

--- calhack.js
+++ calhack.js
@@ -1,16 +1,73 @@
 
 $(document).ready(function(){
-    $('#search').click(function(){
-        generateAirport(46.6734, -71.7412, function(airport) {
-            search(airport, function(count) {
-                something(count);
-            });
-        });
-    });
-    function something(c) {
-        document.write(c);
-    }  
+    // $('#search').click(function(){
+    //     generateAirport(46.6734, -71.7412, function(airport) {
+    //         search(airport, function(count) {
+    //             something(count);
+    //         });
+    //     });
+    // });
+    // function something(c) {
+    //     document.write(c);
+    // }  
+    // var cp = new cityPriority();
 });
+
+class cityPriority{
+    constructor(){
+        this.priority = 0;
+    }
+    retrievepriority(){
+        return this.priority;
+    }
+    clear(){
+        this.priority = 0;
+    }
+    addflight(num){
+        this.priority+=num*0.5;
+    }
+    addhotel(num){
+        this.priority += num*0.2;
+    }
+    addpoi(num){
+        this.priority += num*0.3;
+    }
+}
+var cp = new cityPriority();
+
+//sample call of adding flight priority to the current cp;
+generateAirport(46.6734, -71.7412, function(airport) {
+    search(airport, function(count) {
+        cp.addflight(count);
+        // document.write(cp.retrievepriority());
+    });
+});
+//sample call of adding hotel priority to the current cp;
+returnHotelNum(46.6734,-71.7412);
+//always and only console.log inside function to check the value
+//don't forget to call cp.clear() after retrieve the priority
+//otherwise please reinitialize another instance
+
+
+
+//testing of the cp class
+
+// var cp = new cityPriority();
+// document.write(cp.priority);
+
+// cp.addflight(10);
+// document.write(cp.priority);
+
+// cp.addhotel(10);
+// document.write(cp.priority);
+
+// cp.addpoi(10);
+// document.write(cp.priority);
+
+// document.write(""+cp.retrievepriority());
+// cp.clear();
+// document.write(cp.priority);
+
 
 //pass in the city to generate popularity
 function search(s, callback) {
@@ -62,6 +119,35 @@ function generateAirport(latitude, longitude, callback){
 
 
 }
+
+//num is the number of hotel numbers 
+ function returnHotelNum(latitude,longitude){   
+    //var s=0;
+    $.ajax({
+    url : "https://api.sandbox.amadeus.com/v1.2/hotels/search-circle",
+    type : 'GET', 
+    dataType: 'json',
+    data: {"apikey": "j1QJS395Gyixso59Esaj4apISXiLqAMj", 
+        "latitude": latitude,
+        "longitude": longitude,
+        "radius"  :"20",
+        "check_in" :"2016-12-25", 
+        "check_out" :'2016-12-26'},
+    })
+    .done(function(result) {       
+        var s  = result.results.length;
+        // console.log(s);
+        // getHotelNum(s);
+        cp.addhotel(s);
+    })
+    .fail(function() {
+        console.log("error");
+    })
+    .always(function() {
+        console.log("complete");
+    });;
+}  
+
 //returns date of a specific day 
 // var d = new Date();
 // var year = d.getFullYear();
@@ -93,3 +179,4 @@ function createPeriod(year1, month1, day1, year2, month2, day2) {
 
     return output;
 }
+
